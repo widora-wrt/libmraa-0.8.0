d7bfa84b539b6aa6806698da8f711a71e936e2d7
tongjinlv <179990830@qq.com>
2019-04-20 20:29:03 +0800

    no message

--- src/lcd/lcd.c
+++ src/lcd/lcd.c
@@ -1136,11 +1136,9 @@ mraa_lcd_drawawesome_index(mraa_lcd_context dev,uint16 size,uint16 x,uint16 y,in
   FT_Set_Transform( face, &matrix, &pen );
   error = FT_Load_Char( face, 0xf000+ind, FT_LOAD_RENDER );
   if(error)printf("font error=%d\r\n",ind);
-  if (error) continue;       
   mraa_lcd_draw_bitmap(dev,size,&slot->bitmap,slot->bitmap_left,target_height-target_height*13/100-slot->bitmap_top );
   pen.x += slot->advance.x;
   pen.y += slot->advance.y;
-  free(chinese_char);
   mraa_lcd_draw_image(dev,size,slot->bitmap_left+slot->bitmap.width,x,y,color_f,color_b,color_a);
   FT_Done_Face(face);
   FT_Done_FreeType(library);
